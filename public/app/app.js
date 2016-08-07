e256c29d66245f58bf02869f2f11bce13ca7920c
alexwander <ryndyuk.alexander@gmail.com>
2016-08-07 16:56:10 +0300

    changed some files

--- public/app/app.js
+++ public/app/app.js
@@ -4,7 +4,6 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $routeProvider
     .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
-
 });
 
 angular.module('app').controller('mainCtrl', function($scope) {
